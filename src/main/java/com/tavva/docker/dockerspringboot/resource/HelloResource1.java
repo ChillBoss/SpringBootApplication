61c0b58479e3b5f09e5e34ad59d3942012493e3c
Chaitanya <37467025+chythu03@users.noreply.github.com>
2019-04-10 12:14:42 +0530

    Update HelloResource1.java

--- src/main/java/com/tavva/docker/dockerspringboot/resource/HelloResource1.java
+++ src/main/java/com/tavva/docker/dockerspringboot/resource/HelloResource1.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/rest/docker/chythu")
-public class HelloResource {
+public class HelloResource1 {
 
     @GetMapping
     public String hello(){
